e8de7bda37e26d5c165ed6d6f4da48bb2c113caf
anbe6083 <anbe6083@colorado.edu>
2017-12-01 14:53:19 -0800

    Added Links for react-router

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Balance from './Balance';
+import {Link} from 'react-router-dom';
 export default class TopBar extends Component {
     constructor(props) {
         super(props);
@@ -10,8 +11,12 @@ export default class TopBar extends Component {
         return (
             <div className="TopBarContainer">
                 <div className='navBar'>
-                    <a href='#'>Budgeted Categories</a>
-                    <a href='#'>Account</a>
+                    <ul>
+                        <li> <Link to={`/`}> Account </Link> </li>
+                        <li><Link to={`/budget`}> Budgeted Categories </Link> </li>
+                    </ul>
+                    {/* <a href='#'>Budgeted Categories</a>
+                    <a href='#'>Account</a> */}
                 </div>
                 <Balance balance={this.props.balance} label={this.props.label}/>
 
