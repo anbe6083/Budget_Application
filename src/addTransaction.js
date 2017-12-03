aebaa02850cfd4f3d0b47c1f222b62d226d259ba
anbe6083 <anbe6083@colorado.edu>
2017-12-03 14:38:36 -0800

    Added comments. Also added onChange event handler for the user input

--- src/addTransaction.js
+++ src/addTransaction.js
@@ -8,12 +8,12 @@ export default class AddTransactionRow extends Component {
             open: false
         }
     }
-
+    //Whenever the user presses the "Add a transaction" button, change the state 'open' to open the modal
     onOpenModal = () => {
         console.log('open modal');
         this.setState({ open: true });
     };
-
+    //Whenever the user presses the "Submit" button, change the state 'open' to close the modal
     onCloseModal = () => {
         this.setState({ open: false });
     };
@@ -21,14 +21,16 @@ export default class AddTransactionRow extends Component {
     render() {
         const { open } = this.state;
         return (
+            //This holds the row where the user can add a new transaction
+            // The user can enter the transaction data in the modal that pops up
             <div className='addTransactionRow'>
                 <button id="addTransactionBtn" onClick={this.onOpenModal} type="button">Add A Transaction</button>
                 <Modal open={open} onClose={this.onCloseModal} >
                     <h2>Add a Transaction</h2>
-                    Date: <input type='date' /> <br />
-                    Payee: <input type='text' name="Payee"/> <br />
-                    Inflow: <input type='text' name="Inflow"/><br />
-                    Outflow: <input type='text' name="Outflow"/><br />
+                    Date: <input type='date' onChange={this.handleChange} /> <br />
+                    Payee: <input type='text' name="Payee" onChange={this.handleChange}/> <br />
+                    Inflow: <input type='text' name="Inflow" onChange={this.handleChange}/><br />
+                    Outflow: <input type='text' name="Outflow" onChange={this.handleChange}/><br />
                 </Modal>
             </div>
         )
